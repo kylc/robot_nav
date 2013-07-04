f4331f9f825140597cc3a7cea060e5beda955950
Kyle Cesare <kcesare@gmail.com>
2013-07-03 22:27:39 -0700

    Move code between movement/skeleton modules.

--- navigation/movement.py
+++ navigation/movement.py
@@ -1,14 +1,6 @@
 import numpy as np
-from skimage import feature, graph
-import movement
-
-TEMPLATE_CENTER_OFFSET = 1
-BASE_VERT_TEMPLATE = np.array([[0, 1, 0],
-                               [0, 1, 0],
-                               [0, 0, 0]])
-BASE_DIAG_TEMPLATE = np.array([[1, 0, 0],
-                               [0, 1, 0],
-                               [0, 0, 0]])
+from skimage import graph
+import skeleton
 
 class Path:
     def __init__(self, start, end, points, cost):
@@ -32,40 +24,6 @@ class Path:
                 else:
                     return path_as_points[-1]
 
-def make_endpoint_templates():
-    """Return the endpoint matching template matricies."""
-    templates = []
-
-    for i in xrange(0, 4):
-        templates.append(np.rot90(BASE_VERT_TEMPLATE, k=i))
-        templates.append(np.rot90(BASE_DIAG_TEMPLATE, k=i))
-
-    return templates
-
-def offset_template_point(p):
-    return p + TEMPLATE_CENTER_OFFSET
-
-def swap_axes((x, y)):
-    return (y, x)
-
-def find_endpoints(skel):
-    """Return all matched endpoints in the image."""
-    endpoints = []
-
-    for template in make_endpoint_templates():
-        matches = feature.match_template(skel, template)
-
-        # Filter to only those that match with an 80% probability
-        matches = np.where(matches > 0.80)
-
-        # skimage matches on the top left of the template, so shift it to the
-        # center point
-        matches = map(offset_template_point, matches)
-
-        endpoints.extend(map(swap_axes, zip(*matches)))
-
-    return endpoints
-
 def find_path(start, end, skel):
     """Find the shortest path along the skeleton from point to point."""
     # TODO: Why do matplotlib/skimage not agree on array shapes?
@@ -81,9 +39,9 @@ def find_path(start, end, skel):
 def find_all_paths(start, skel):
     paths = []
 
-    endpoints = find_endpoints(skel)
+    endpoints = skeleton.find_endpoints(skel)
     for endpoint in endpoints:
-        xs, ys, cost = movement.find_path(start, endpoint, skel)
+        xs, ys, cost = find_path(start, endpoint, skel)
 
         path = Path(start, endpoint, [xs, ys], cost)
         paths.append(path)
--- navigation/skeleton.py
+++ navigation/skeleton.py
@@ -1,6 +1,15 @@
 import numpy as np
 from scipy import stats
-from skimage import morphology
+from skimage import feature, morphology
+
+TEMPLATE_CENTER_OFFSET = 1
+BASE_VERT_TEMPLATE = np.array([[0, 1, 0],
+                               [0, 1, 0],
+                               [0, 0, 0]])
+BASE_DIAG_TEMPLATE = np.array([[1, 0, 0],
+                               [0, 1, 0],
+                               [0, 0, 0]])
+
 
 def make_skeleton(image, mindist):
     skel, distance = morphology.medial_axis(image, return_distance=True)
@@ -19,3 +28,37 @@ def filter_skeleton_distances(dist_on_skel, mindist=10):
 def normalize_skeleton(dist_on_skel):
     """Return the skeleton with all values normalized to 1."""
     return np.where(dist_on_skel > 0, 1, -1)
+
+def make_endpoint_templates():
+    """Return the endpoint matching template matricies."""
+    templates = []
+
+    for i in xrange(0, 4):
+        templates.append(np.rot90(BASE_VERT_TEMPLATE, k=i))
+        templates.append(np.rot90(BASE_DIAG_TEMPLATE, k=i))
+
+    return templates
+
+def offset_template_point(p):
+    return p + TEMPLATE_CENTER_OFFSET
+
+def swap_axes((x, y)):
+    return (y, x)
+
+def find_endpoints(skel):
+    """Return all matched endpoints in the image."""
+    endpoints = []
+
+    for template in make_endpoint_templates():
+        matches = feature.match_template(skel, template)
+
+        # Filter to only those that match with an 80% probability
+        matches = np.where(matches > 0.80)
+
+        # skimage matches on the top left of the template, so shift it to the
+        # center point
+        matches = map(offset_template_point, matches)
+
+        endpoints.extend(map(swap_axes, zip(*matches)))
+
+    return endpoints
