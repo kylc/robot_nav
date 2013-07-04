734c5d4e165f28a555e1902825676f3f91e07ea1
Kyle Cesare <kcesare@gmail.com>
2013-07-03 20:49:12 -0700

    Move animation test to it's own file.

---
+++ test/anim_test.py
@@ -0,0 +1,68 @@
+import sys
+from random import choice
+import pygame
+import numpy as np
+
+from image_loader import load_image
+from navigation import movement, skeleton
+
+# Load the image
+print "Loading image..."
+data = load_image(sys.argv[1])
+
+print "Generating skeleton..."
+skel = skeleton.make_skeleton(data, mindist=10)
+
+# Compute the paths
+print "Finding paths..."
+location = (1, 132)
+
+paths = movement.find_all_paths(location, skel)
+
+EMPTY_COLOR = (255, 255, 255)
+OBSTACLE_COLOR = (0, 0, 0)
+ROBOT_COLOR = (255, 0, 0,)
+
+def draw_image(screen, img):
+    for (y, x), v in np.ndenumerate(img):
+        screen.fill((v, v, v), rect=(x, y, 1, 1))
+
+pygame.init()
+screen = pygame.display.set_mode((data.shape[1], data.shape[0]))
+
+background = pygame.Surface(screen.get_size())
+background = background.convert()
+draw_image(background, data)
+
+# The visited endpoints, so we know not to go back to them
+visited = []
+
+clock = pygame.time.Clock()
+while True:
+    clock.tick(60)
+
+    screen.blit(background, (0, 0))
+
+    # Draw the robot
+    locx, locy = location
+    screen.fill(ROBOT_COLOR, rect=(locx - 5, locy - 5, 10, 10))
+
+    closest_path = movement.find_closest_path(location, paths, visited)
+
+    path_as_points = zip(*closest_path.path)
+    for idx, point in enumerate(path_as_points):
+        # If this is our current location in the path, move to the next point
+        if location == point:
+            # If there are any more points in the path, move to them
+            if len(path_as_points) > idx + 1:
+                location = path_as_points[idx + 1]
+                break
+            # If this is the end, we've now visited the endpoint, so mark it as
+            # so
+            else:
+                visited.append((location[1], location[0]))
+
+                # Now figure out how to get to all the other endpoints
+                paths = movement.find_all_paths(location, skel)
+
+    pygame.display.flip()
--- test/image_test.py
+++ test/image_test.py
@@ -1,8 +1,7 @@
 import sys
 from random import choice
-import pygame
 import numpy as np
-
+import matplotlib.pyplot as plt
 from image_loader import load_image
 from navigation import movement, skeleton
 
@@ -13,56 +12,29 @@ data = load_image(sys.argv[1])
 print "Generating skeleton..."
 skel = skeleton.make_skeleton(data, mindist=10)
 
+# Draw!
+plt.figure()
+plt.imshow(data, cmap=plt.cm.gray, interpolation='nearest')
+
 # Compute the paths
+print "Finding endpoints..."
+endpoints = movement.find_endpoints(skel)
+
 print "Finding paths..."
-location = (1, 132)
+start = (1, 132)
 
-paths = movement.find_all_paths(location, skel)
+paths = movement.find_all_paths(start, skel)
+closest_path = movement.find_closest_path(start, paths)
 
-EMPTY_COLOR = (255, 255, 255)
-OBSTACLE_COLOR = (0, 0, 0)
-ROBOT_COLOR = (255, 0, 0,)
+print "Plotting!"
+for path in paths:
+    color = np.random.rand(3)
 
-def draw_image(screen, img):
-    for (y, x), v in np.ndenumerate(img):
-        screen.fill((v, v, v), rect=(x, y, 1, 1))
+    xs, ys = path.path
 
-pygame.init()
-screen = pygame.display.set_mode((data.shape[1], data.shape[0]))
+    plt.plot(path.end[1], path.end[0], color=color, marker='o')
+    plt.plot(path.path[0], path.path[1], color=color, marker=',')
 
-background = pygame.Surface(screen.get_size())
-background = background.convert()
-draw_image(background, data)
+plt.plot(closest_path.path[0], closest_path.path[1], color='r', linewidth=5)
 
-# The visited endpoints, so we know not to go back to them
-visited = []
-
-clock = pygame.time.Clock()
-while True:
-    clock.tick(60)
-
-    screen.blit(background, (0, 0))
-
-    # Draw the robot
-    locx, locy = location
-    screen.fill(ROBOT_COLOR, rect=(locx - 5, locy - 5, 10, 10))
-
-    closest_path = movement.find_closest_path(location, paths, visited)
-
-    path_as_points = zip(*closest_path.path)
-    for idx, point in enumerate(path_as_points):
-        # If this is our current location in the path, move to the next point
-        if location == point:
-            # If there are any more points in the path, move to them
-            if len(path_as_points) > idx + 1:
-                location = path_as_points[idx + 1]
-                break
-            # If this is the end, we've now visited the endpoint, so mark it as
-            # so
-            else:
-                visited.append((location[1], location[0]))
-
-                # Now figure out how to get to all the other endpoints
-                paths = movement.find_all_paths(location, skel)
-
-    pygame.display.flip()
+plt.show()
